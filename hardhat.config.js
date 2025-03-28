c9fce6368f10094ecc35dd458ad31ed1cdc0a1b3
shreyakatare7 <160975793+shreyakatare7@users.noreply.github.com>
2025-03-27 23:31:15 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      // Hardhat's default network configuration for local node
+      accounts: {
+        // Create 50 accounts with 10000 ether each
+        count: 50, // Number of accounts
+        initialBalance: "10000000000000000000000", // 10,000 ETH in wei (10000 ether)
+      }
+    }
+  }
 };
